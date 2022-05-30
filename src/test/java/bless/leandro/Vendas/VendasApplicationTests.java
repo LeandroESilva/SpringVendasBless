50e9b4836b0a96400b51a6ad25a26162cbab0d61
LeandroESilva <78575107+LeandroESilva@users.noreply.github.com>
2022-05-30 19:48:53 -0300

    Iniciando o projeto
    Spring 2.6.8

---
+++ src/test/java/bless/leandro/Vendas/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package bless.leandro.Vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
