da57a1ad83b7b8227c4d82c2c1152b52572dc01c
LeandroESilva <78575107+LeandroESilva@users.noreply.github.com>
2022-06-13 21:28:49 -0300

    criado diretorio e classes domain

---
+++ src/main/java/bless/leandro/Vendas/domain/Produto.java
@@ -0,0 +1,30 @@
+package bless.leandro.Vendas.domain;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class Produto {
+    private  Integer id;
+    private  String descricao;
+    private BigDecimal precoUnitario;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(BigDecimal precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+}
